e8a637c9ca723660b4f0d846dc1d5ab112d5ea1b
Joseph Rosicka <jrosicka@github.com>
2022-12-27 19:02:26 -0600

    AbilityTimer note

--- Assets/Scripts/Gameplay/Entities/Abilities/AbilityTimer.cs
+++ Assets/Scripts/Gameplay/Entities/Abilities/AbilityTimer.cs
@@ -7,6 +7,10 @@ namespace Gameplay.Entities.Abilities {
     /// <summary>
     /// Represents a timer for blocking particular <see cref="AbilityChannel"/> cooldowns - can be checked to see if
     /// <see cref="IAbility"/>s of those same channel are allowed to be used
+    ///
+    /// NOTE: I wanted to avoid needing to network the state of every timer on every frame, so each client handles its own
+    /// timers instead of being given updates by the server. Only the expiration and list adding/removal are networked.
+    /// This may need to be changed if ability timers are too out of sync across different clients. 
     /// </summary>
     public class AbilityTimer {
         public event Action<AbilityTimer> CompletedEvent;
