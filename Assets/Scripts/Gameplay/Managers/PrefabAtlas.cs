eef3eb26c602a7b0ee406a61f17d98437ef5eac2
Joey Rosicka <jrosicka@concretesoftware.com>
2024-06-13 22:02:03 -0500

    Adding PrefabAtlas

---
+++ Assets/Scripts/Gameplay/Managers/PrefabAtlas.cs
@@ -0,0 +1,11 @@
+using Gameplay.Entities;
+using UnityEngine;
+
+namespace Gameplay.Managers {
+    /// <summary>
+    /// Convenient place to configure and access prefabs
+    /// </summary>
+    public class PrefabAtlas : MonoBehaviour {
+        public ArcherArrow ArcherArrow;
+    }
+}
