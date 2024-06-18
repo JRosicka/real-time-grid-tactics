e2cbf1c5d11938013fc7b65241b7846408f384e4
Joey Rosicka <jrosicka@concretesoftware.com>
2024-06-17 22:31:09 -0500

    Income animation

--- Assets/Scripts/Gameplay/Entities/Views/Entity Views/VillageView.cs
+++ Assets/Scripts/Gameplay/Entities/Views/Entity Views/VillageView.cs
@@ -1,16 +1,14 @@
 using Gameplay.Config.Abilities;
 using Gameplay.Entities.Abilities;
-using UnityEngine;
 
 namespace Gameplay.Entities {
     public class VillageView : GridEntityParticularView {
         public IncomeAnimationBehavior IncomeAnimationBehavior;
 
         public override bool DoAbility(IAbility ability, AbilityCooldownTimer cooldownTimer) {
-            Debug.Log($"{nameof(DoAbility)}: {ability}");
             switch (ability.AbilityData) {
-                case IncomeAbilityData _:
-                    IncomeAnimationBehavior.DoIncomeAnimation();
+                case IncomeAbilityData data:
+                    IncomeAnimationBehavior.DoIncomeAnimation(data);
                     return false;
                 default:
                     return true;
--- Assets/Scripts/Gameplay/Entities/Views/IncomeAnimationBehavior.cs
+++ Assets/Scripts/Gameplay/Entities/Views/IncomeAnimationBehavior.cs
@@ -1,9 +1,19 @@
+using Gameplay.Config.Abilities;
+using TMPro;
 using UnityEngine;
 
 namespace Gameplay.Entities {
     public class IncomeAnimationBehavior : MonoBehaviour {
-        public void DoIncomeAnimation() {
-            Debug.Log(nameof(DoIncomeAnimation));
+        [SerializeField] private Animator _animator;
+        [SerializeField] private TMP_Text _incomeText;
+        
+        [SerializeField] private Color _textColor;
+        [SerializeField] private string _incomeTextFormat = "+{0}";
+        
+        public void DoIncomeAnimation(IncomeAbilityData data) {
+            _incomeText.text = string.Format(_incomeTextFormat, data.ResourceAmountIncome.Amount);
+            _incomeText.color = _textColor;
+            _animator.Play("ShowIncome");
         }
     }
 }
