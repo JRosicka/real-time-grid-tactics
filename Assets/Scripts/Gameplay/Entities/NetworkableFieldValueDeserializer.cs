b26d0170eb5c64f41e9cdf1e3d7a95357d6a49fc
Joseph Rosicka <jrosicka@github.com>
2025-02-11 17:21:56 -0600

    Fixing deserializer reference for NetworkableGridEntityValue

--- Assets/Scripts/Gameplay/Entities/NetworkableFieldValueDeserializer.cs
+++ Assets/Scripts/Gameplay/Entities/NetworkableFieldValueDeserializer.cs
@@ -13,7 +13,7 @@ namespace Gameplay.Entities {
                 nameof(NetworkableVector2IntegerValue) => NetworkableVector2IntegerValue.Deserialize(reader),
                 nameof(ResourceAmount) => ResourceAmount.Deserialize(reader),
                 nameof(TargetLocationLogic) => TargetLocationLogic.Deserialize(reader),
-                nameof(GridEntity.LastAttackedEntity) => NetworkableGridEntityValue.Deserialize(reader),
+                nameof(NetworkableGridEntityValue) => NetworkableGridEntityValue.Deserialize(reader),
                 _ => throw new Exception($"No deserializer found for {nameof(INetworkableFieldValue)} with ID {id}")
             };
         }
