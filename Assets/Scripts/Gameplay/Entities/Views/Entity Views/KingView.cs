1fbbb3e23a516d2e7862713d35db3d03b2356a6e
Joey Rosicka <jrosicka@concretesoftware.com>
2025-10-04 14:49:33 -0500

    Adding parade animation text

--- Assets/Scripts/Gameplay/Entities/Views/Entity Views/KingView.cs
+++ Assets/Scripts/Gameplay/Entities/Views/Entity Views/KingView.cs
@@ -1,14 +1,26 @@
+using Gameplay.Config.Abilities;
 using Gameplay.Entities.Abilities;
+using UnityEngine;
 
 namespace Gameplay.Entities {
     public class KingView : GridEntityParticularView {
+        [SerializeField] private Animator _paradeTextAnimator;
+        
         public override void Initialize(GridEntity entity) { }
         public override void LethalDamageReceived() { }
 
         public override bool DoAbility(IAbility ability, AbilityCooldownTimer cooldownTimer) {
-            return ability.AbilityData switch {
-                _ => true
-            };
+            switch (ability.AbilityData) {
+                case ParadeAbilityData _:
+                    DoParadeAnimation();
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private void DoParadeAnimation() {
+            _paradeTextAnimator.Play("ParadeActive");
         }
     }
 }
