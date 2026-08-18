a82d60f1549c6fa44be58473c8d64ec2b873141d
Joseph Rosicka <jrosicka@github.com>
2026-08-17 21:39:40 -0500

    DiscordManager boilerplate

---
+++ Assets/Scripts/Network/Discord/DiscordManager.cs
@@ -0,0 +1,10 @@
+using Discord.Sdk;
+using UnityEngine;
+
+namespace Game.Network.Discord {
+    public class DiscordManager : MonoBehaviour {
+        private const ulong ClientId = 1539068505301721138;
+
+        private Client client;
+    }
+}
